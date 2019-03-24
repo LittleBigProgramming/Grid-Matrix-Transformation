87ca7d44c863e849aabfb2991c83f48bcf63e91b
Andy Alban <20643587+LittleBigProgramming@users.noreply.github.com>
2019-03-24 19:30:33 +0000

    changed current implementation of various Vectors to implement a more effecient equation using a matrix

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -4,38 +4,45 @@ public class Rotate : Transformation
 {
     public Vector3 mRotation;
 
-    public override Vector3 Apply(Vector3 point) {
-        float radX = mRotation.x * Mathf.Deg2Rad;
-        float radY = mRotation.y * Mathf.Deg2Rad;
-        float radZ = mRotation.z * Mathf.Deg2Rad;
+    public override Matrix4x4 Matrix {
+        get {
+            float radX = mRotation.x * Mathf.Deg2Rad;
+            float radY = mRotation.y * Mathf.Deg2Rad;
+            float radZ = mRotation.z * Mathf.Deg2Rad;
 
-        float sinX = Mathf.Sin(radX);
-        float cosX = Mathf.Cos(radX);
+            float sinX = Mathf.Sin(radX);
+            float cosX = Mathf.Cos(radX);
 
-        float sinY = Mathf.Sin(radY);
-        float cosY = Mathf.Cos(radY);
+            float sinY = Mathf.Sin(radY);
+            float cosY = Mathf.Cos(radY);
 
-        float sinZ = Mathf.Sin(radZ);
-        float cosZ = Mathf.Cos(radZ);
+            float sinZ = Mathf.Sin(radZ);
+            float cosZ = Mathf.Cos(radZ);
 
-        Vector3 xAxis = new Vector3(
-            cosY * cosY,
-            cosX * sinZ + sinX * sinY * cosZ,
-            sinX * sinZ - cosX * sinY * cosZ
-        );
+            Matrix4x4 matrix = new Matrix4x4();
+            matrix.SetColumn(0, new Vector4(
+                cosY * cosY,
+                cosX * sinZ + sinX * sinY * cosZ,
+                sinX * sinZ - cosX * sinY * cosZ,
+                0.0f
+            ));
 
-        Vector3 yAxis = new Vector3(
-            -cosY * sinZ,
-            cosX * cosZ - sinX * sinY * sinZ,
-            sinX * cosZ + cosX * sinY * sinZ
-        );
+            matrix.SetColumn(1, new Vector4(
+                -cosY * sinZ,
+                cosX * cosZ - sinX * sinY * sinZ,
+                sinX * cosZ + cosX * sinY * sinZ,
+                0.0f
+            ));
 
-        Vector3 zAxis = new Vector3(
-            sinY,
-            -sinX * cosY,
-            cosX * cosY
-        );
+            matrix.SetColumn(2, new Vector4(
+                sinY,
+                -sinX * cosY,
+                cosX * cosY,
+                0.0f
+            ));
 
-        return xAxis * point.x + yAxis * point.y + zAxis * point.z;
+            matrix.SetColumn(3, new Vector4(0f, 0f, 0f, 1f));
+            return matrix;
+        }
     }
 }
--- Assets/Scripts/Scale.cs
+++ Assets/Scripts/Scale.cs
@@ -4,11 +4,16 @@ public class Scale :  Transformation {
 
     public Vector3 mScale;
 
-    public override Vector3 Apply(Vector3 point) {
-        point.x *= mScale.x;
-        point.y *= mScale.y;
-        point.z *= mScale.z;
-
-        return point;
+    public override Matrix4x4 Matrix
+    {
+        get
+        {
+            Matrix4x4 matrix = new Matrix4x4();
+            matrix.SetRow(0, new Vector4(mScale.x, 0f, 0f, 0f));
+            matrix.SetRow(1, new Vector4(0f, mScale.y, 0f, 0f));
+            matrix.SetRow(2, new Vector4(0f, 0f, mScale.z, 0f));
+            matrix.SetRow(3, new Vector4(0f, 0f, 0f, 1f));
+            return matrix;
+        }
     }
 }
