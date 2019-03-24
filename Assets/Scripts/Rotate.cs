0019cfd27f38667d6532d930f5df90b613c331e3
Andy Alban <20643587+LittleBigProgramming@users.noreply.github.com>
2019-03-24 17:03:14 +0000

    implemented rotation for each axis splitting each into a Vector3f and returning the result

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -5,14 +5,37 @@ public class Rotate : Transformation
     public Vector3 mRotation;
 
     public override Vector3 Apply(Vector3 point) {
+        float radX = mRotation.x * Mathf.Deg2Rad;
+        float radY = mRotation.y * Mathf.Deg2Rad;
         float radZ = mRotation.z * Mathf.Deg2Rad;
+
+        float sinX = Mathf.Sin(radX);
+        float cosX = Mathf.Cos(radX);
+
+        float sinY = Mathf.Sin(radY);
+        float cosY = Mathf.Cos(radY);
+
         float sinZ = Mathf.Sin(radZ);
         float cosZ = Mathf.Cos(radZ);
 
-        return new Vector3(
-            point.x * cosZ - point.y * sinZ,
-            point.x * sinZ + point.y * cosZ,
-            point.z
+        Vector3 xAxis = new Vector3(
+            cosY * cosY,
+            cosX * sinZ + sinX * sinY * cosZ,
+            sinX * sinZ - cosX * sinY * cosZ
         );
+
+        Vector3 yAxis = new Vector3(
+            -cosY * sinZ,
+            cosX * cosZ - sinX * sinY * sinZ,
+            sinX * cosZ + cosX * sinY * sinZ
+        );
+
+        Vector3 zAxis = new Vector3(
+            sinY,
+            -sinX * cosY,
+            cosX * cosY
+        );
+
+        return xAxis * point.x + yAxis * point.y + zAxis * point.z;
     }
 }
