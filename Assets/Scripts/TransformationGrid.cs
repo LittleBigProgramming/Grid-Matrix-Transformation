0cfc10b44d932df925ec14a05d9cdb72abef3d4a
Andy Alban <20643587+LittleBigProgramming@users.noreply.github.com>
2019-03-24 14:30:47 +0000

    Added implementation for basic transformation and translation, which is used within the TransformationGrid object

--- Assets/Scripts/TransformationGrid.cs
+++ Assets/Scripts/TransformationGrid.cs
@@ -7,9 +7,11 @@ public class TransformationGrid : MonoBehaviour {
     public Transform mPrefab;
     public int mGridResolution = 10;
     Transform[] mGrid;
+    List<Transformation> mTransformations;
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
         mGrid = new Transform[mGridResolution * mGridResolution * mGridResolution];
 
         for (int i = 0, z = 0; z < mGridResolution; z++) {
@@ -19,12 +21,25 @@ public class TransformationGrid : MonoBehaviour {
                 }
             }
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+        mTransformations = new List<Transformation>();
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        GetComponents<Transformation>(mTransformations);
+        for (int i = 0, z = 0; z < mGridResolution; z++)
+        {
+            for (int y = 0; y < mGridResolution; y++)
+            {
+                for (int x = 0; x < mGridResolution; x++, i++)
+                {
+                    mGrid[i].localPosition = TransformPoint(x, y, z);
+                }
+            }
+        }
+    }
 
     Transform CreateGridPoint (int x, int y, int z) {
         Transform point = Instantiate<Transform>(mPrefab);
@@ -46,5 +61,15 @@ public class TransformationGrid : MonoBehaviour {
             y - (mGridResolution - 1) * 0.5f,
             z - (mGridResolution - 1) * 0.5f
         );
+    }
+
+    Vector3 TransformPoint (int x, int y, int z) {
+        Vector3 coordinates = GetCoordinates(x, y, z);
+
+        for (int i = 0; i < mTransformations.Count; i++) {
+            coordinates = mTransformations[i].Apply(coordinates);
+        }
+
+        return coordinates;
     }
 }
