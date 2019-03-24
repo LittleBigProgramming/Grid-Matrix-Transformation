1762eaadaee1057eb36d45dde29fef5181f3a5fb
Andy Alban <20643587+LittleBigProgramming@users.noreply.github.com>
2019-03-24 16:04:05 +0000

    Added script to handle ONLY Z axis rotation transformations and added it to the grid transformation object

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Rotate : Transformation
+{
+    public Vector3 mRotation;
+
+    public override Vector3 Apply(Vector3 point) {
+        float radZ = mRotation.z * Mathf.Deg2Rad;
+        float sinZ = Mathf.Sin(radZ);
+        float cosZ = Mathf.Cos(radZ);
+
+        return new Vector3(
+            point.x * cosZ - point.y * sinZ,
+            point.x * sinZ + point.y * cosZ,
+            point.z
+        );
+    }
+}
