bb60458265d0890567820e3f37fe40ec12fb5ab0
Andy Alban <20643587+LittleBigProgramming@users.noreply.github.com>
2019-03-24 15:48:44 +0000

    Added script to handle scale transformations and added it to the grid transformation object

---
+++ Assets/Scripts/Scale.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Scale :  Transformation {
+
+    public Vector3 mScale;
+
+    public override Vector3 Apply(Vector3 point) {
+        point.x *= mScale.x;
+        point.y *= mScale.y;
+        point.z *= mScale.z;
+
+        return point;
+    }
+}
